0c35d3c5831a35182da1961016e92bf313c6335b
perkashlal <parkashlal450@gmail.com>
2026-06-05 14:44:46 +0200

    Red: failing Appointment toString test

---
+++ src/main/java/com/examples/cyclerepair/model/Appointment.java
@@ -0,0 +1,9 @@
+package com.examples.cyclerepair.model;
+
+public class Appointment {
+
+	public Appointment(String id, String customerName, String cycleModel, String repairIssue,
+			String appointmentDate) {
+
+	}
+}
---
+++ src/test/java/com/examples/cyclerepair/model/AppointmentTest.java
@@ -0,0 +1,18 @@
+package com.examples.cyclerepair.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class AppointmentTest {
+
+	@Test
+	public void testToString() {
+		Appointment appointment = new Appointment("1", "Mario Rossi", "Road Bike",
+				"Brake adjustment", "2026-06-10");
+
+		assertThat(appointment.toString())
+			.isEqualTo("Appointment [id=1, customerName=Mario Rossi, cycleModel=Road Bike, "
+					+ "repairIssue=Brake adjustment, appointmentDate=2026-06-10]");
+	}
+}

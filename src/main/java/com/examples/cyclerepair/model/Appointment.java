ff4be05958c10bb0eec719713de3acdd88262b4a
perkashlal <parkashlal450@gmail.com>
2026-06-05 14:50:13 +0200

    Green: Appointment toString

--- src/main/java/com/examples/cyclerepair/model/Appointment.java
+++ src/main/java/com/examples/cyclerepair/model/Appointment.java
@@ -2,8 +2,25 @@ package com.examples.cyclerepair.model;
 
 public class Appointment {
 
+	private String id;
+	private String customerName;
+	private String cycleModel;
+	private String repairIssue;
+	private String appointmentDate;
+
 	public Appointment(String id, String customerName, String cycleModel, String repairIssue,
 			String appointmentDate) {
+		this.id = id;
+		this.customerName = customerName;
+		this.cycleModel = cycleModel;
+		this.repairIssue = repairIssue;
+		this.appointmentDate = appointmentDate;
+	}
 
+	@Override
+	public String toString() {
+		return "Appointment [id=" + id + ", customerName=" + customerName + ", cycleModel="
+				+ cycleModel + ", repairIssue=" + repairIssue + ", appointmentDate="
+				+ appointmentDate + "]";
 	}
 }
